295dfc234f8f8633022985f65d326b85fa265046
xayrullaevziyovuddin <xayrullaevziyovuddin>
2023-08-07 15:51:01 +0500

    skills

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -1,15 +1,37 @@
 <template>
-    <div>
-<h1>Skills</h1>
+    <div class="content__skills">
+        <div class="section content">
+
+            <div class="container">
+                <h1 class="title-1">Skills</h1>
+
+                <ul class="content-list">
+                    <li class="content-list__item">
+                        <h2 class="title-2">Frontend</h2>
+                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn,
+                            TailwindCSS, StyledComponents</p>
+                    </li>
+                    <li class="content-list__item">
+                        <h2 class="title-2">Backend</h2>
+                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                    </li>
+                </ul>
+
+            </div>
+
+
+          
+        </div>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Footer from '@/components/Footer.vue';
+
+export default {
+    components: { Footer }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ export default createStore({
   actions: {
     async fetchRepositories({ commit }) {
       try {
-        const response = await axios.get('https://api.github.com/users/xayrullaevziyovuddin/repos');
+        const response = await axios.get('https://api.github.com/users/levi/repos');
         const repositories = response.data;
         commit('setRepositories', repositories);
       } catch (error) {
@@ -23,3 +23,40 @@ export default createStore({
     }
   }
 });
+
+/* // store/index.js
+import { createStore } from 'vuex';
+import axios from 'axios';
+
+export default createStore({
+  state: {
+    repositories: []
+  },
+  mutations: {
+    setRepositories(state, repositories) {
+      state.repositories = repositories;
+    }
+  },
+  actions: {
+    async fetchRepositories({ commit }) {
+      try {
+        let page = 1;
+        let repositories = [];
+
+        while (true) {
+          const response = await axios.get(`https://api.github.com/users/levi/repos?page=${page}`);
+          if (response.data.length === 0) {
+            break; // No more repositories to fetch
+          }
+          repositories = repositories.concat(response.data);
+          page++;
+        }
+
+        commit('setRepositories', repositories);
+      } catch (error) {
+        console.error('Ошибка произошла при получении данных ' + error);
+      }
+    }
+  }
+});
+ */
