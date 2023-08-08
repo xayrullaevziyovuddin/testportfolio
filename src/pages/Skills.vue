d5c07be12d631822d5aacf94c1eb1bbae5d0fabf
xayrullaevziyovuddin <xayrullaevziyovuddin@gmail.com>
2023-08-08 07:05:39 +0500

    end-end

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -8,13 +8,10 @@
                 <ul class="content-list">
                     <li class="content-list__item">
                         <h2 class="title-2">Frontend</h2>
-                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn,
+                        <p>JavaScript, TypeScript, ReactJS, Vue js , Vux , Pinia , Redux , HTML, CSS, Scss, NPM, BootStrap, MaterialUI, Yarn,
                             TailwindCSS, StyledComponents</p>
                     </li>
-                    <li class="content-list__item">
-                        <h2 class="title-2">Backend</h2>
-                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-                    </li>
+                    
                 </ul>
 
             </div>
